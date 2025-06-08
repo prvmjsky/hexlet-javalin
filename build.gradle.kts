c6c25c4f3d9169496f4d9c02bb568ab18fac325a
prvmjsky <lukyanovalexey1@gmail.com>
2025-06-08 12:59:25 +0500

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,8 @@ dependencies {
 
     implementation("io.javalin:javalin:6.6.0")
     implementation("org.slf4j:slf4j-simple:2.0.7")
-    implementation("io.javalin:javalin-rendering:6.1.3")
-    implementation("gg.jte:jte:3.1.9")
+    implementation("io.javalin:javalin-rendering:6.6.0")
+    implementation("gg.jte:jte:3.2.1")
 
     compileOnly("org.projectlombok:lombok:1.18.36")
     annotationProcessor("org.projectlombok:lombok:1.18.36")
