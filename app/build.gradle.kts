4feb3ba48ec780007eb46e6b406eae252350d543
prvmjsky <lukyanovalexey1@gmail.com>
2025-06-25 13:18:48 +0500

    add postgresql dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
 
     implementation("com.h2database:h2:2.2.220")
     implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("org.postgresql:postgresql:42.7.7")
 }
 
 tasks.test {
